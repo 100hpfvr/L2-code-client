cb3f5c8a14a4df2c073af0d5871c8a5705422330
mateus <mateus.bellomo@prodatamobility.com.br>
2025-02-27 15:02:24 -0300

    Adiciona componente ContactList com botão usando PrimeNG

---
+++ src/app/pages/contact-list/contact-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {Button} from 'primeng/button';
+
+@Component({
+  selector: 'app-contact-list',
+  imports: [
+    Button
+  ],
+  templateUrl: './contact-list.component.html',
+  styleUrl: './contact-list.component.css'
+})
+export class ContactListComponent {
+
+}
